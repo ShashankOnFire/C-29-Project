03b1b466233a2c57ae0fae5a475f4fdaff765d8a
ShashankOnFire <64587455+ShashankOnFire@users.noreply.github.com>
2020-06-16 22:23:32 +0530

    Update Block.js

--- js/Block.js
+++ js/Block.js
@@ -1,8 +1,6 @@
 class Block{
     constructor(x, y, width, height) {
         var options = {
-           
-            restitution :0.4,
             friction :0.0,
             //isStatic:true
            
